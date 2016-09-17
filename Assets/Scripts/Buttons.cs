b947d9432757440685ae6c04cb0eecf7bdb38d1d
balaringabriel <balaringabriel@gmail.com>
2016-09-17 06:32:20 -0300

    All Scenes / Pause
    implement of pause on GameIn Scene
    All Scenes created w/ buttons working

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+	public void Play ()
+	{
+		SceneManager.LoadScene ("GameIn");
+	}
+
+	public void Options ()
+	{
+		SceneManager.LoadScene ("Options");
+	}
+
+	public void Credits ()
+	{
+		SceneManager.LoadScene ("Credits");
+	}
+
+	public void Store ()
+	{
+		SceneManager.LoadScene ("Store");
+	}
+
+	public void ReturnToMenu ()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+
+	public void Restart ()
+	{
+		SceneManager.LoadScene ("GameIn");
+	}
+}
---
+++ Assets/Scripts/Paused.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paused : MonoBehaviour
+{
+	bool pause;
+	public GameObject pauseManager;
+
+	void Start ()
+	{
+		Time.timeScale = 1;
+		pauseManager.gameObject.SetActive (false);
+		pause = true;
+	}
+
+	void Update ()
+	{
+		if (pause) {
+			if (Input.GetKeyUp (KeyCode.P)) {
+				Time.timeScale = 0;
+				pauseManager.gameObject.SetActive (true);
+				pause = false;
+			}
+		} else if (pause == false) {
+			if (Input.GetKeyUp (KeyCode.P)) {
+				Time.timeScale = 1;
+				pauseManager.gameObject.SetActive (false);
+				pause = true;
+			}
+		}
+	}
+}
