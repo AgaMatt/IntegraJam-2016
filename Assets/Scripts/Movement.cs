be11530c6d7cb430e231684fc83d7caab5b84a3f
balaringabriel <balaringabriel@gmail.com>
2016-09-17 02:19:09 -0300

    Score

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,33 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
 	public float forca, kickPage, kickBook;
 	public GameObject player;
 	bool canEat;
 
-	void Start () {
-	//	player = GameObject.FindGameObjectWithTag ("Player");
+	void Start ()
+	{
+		//	player = GameObject.FindGameObjectWithTag ("Player");
 
 	
 	}
-	
 
-	void Update () {
+
+	void Update ()
+	{
 		
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			canEat = true;
-		//	print ("AAAA");
+			//	print ("AAAA");
 			GetComponent<Rigidbody2D> ().AddForce (Vector3.down * forca * Time.deltaTime);
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D coll)
+	void OnTriggerEnter2D (Collider2D coll)
 	{
-		 print ("Colili");
+		print ("Colili");
 		if (canEat) {
-			if (coll.tag == "Page") {
+			if (coll.tag == "Page" || coll.tag == "Book") {
 				print ("AloPAGE");
 				gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * kickPage * Time.deltaTime);
 				Destroy (coll.gameObject);
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	public Text scoreText;
+	public int score;
+
+
+	void OnTriggerEnter2D (Collider2D coll)
+	{
+		
+		if (coll.tag == "Book") {
+			score++;
+			scoreText.text = score.ToString ();
+		}
+	}
+}
