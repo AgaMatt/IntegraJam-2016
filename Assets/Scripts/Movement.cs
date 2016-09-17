b8a7065d7c9dc15abb0ae4273b64c5d42177f585
AgaMatt <heitormattosinho@gmail.com>
2016-09-17 04:10:55 -0300

    Fixing Bounce 1/2

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,23 +4,16 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
 
-	public float forca, kickPage, kickBook;
+	public float forca, kickPage, kickBook, kickFloor;
 	public GameObject player;
-	bool canEat;
-
-	void Start ()
-	{
-		//	player = GameObject.FindGameObjectWithTag ("Player");
-
-	
-	}
+	bool canJump;
 
 
 	void Update ()
 	{
 		
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			canEat = true;
+			canJump = true;
 			//	print ("AAAA");
 			GetComponent<Rigidbody2D> ().AddForce (Vector3.down * forca * Time.deltaTime);
 		}
@@ -28,14 +21,24 @@ public class Movement : MonoBehaviour
 
 	void OnTriggerEnter2D (Collider2D coll)
 	{
-		print ("Colili");
-		if (canEat) {
-			if (coll.tag == "Page" || coll.tag == "Book") {
-				print ("AloPAGE");
+		//print ("Colili");
+		if(coll.tag == "Floor"){
+			canJump = false;
+		}
+		if (canJump) {
+			if (coll.tag == "Page") {
+				//print ("AloPAGE");
 				gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * kickPage * Time.deltaTime);
-				Destroy (coll.gameObject);
-				canEat = false;
+				canJump = false;
+			}else if(coll.tag == "Book"){
+				gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * kickBook * Time.deltaTime);
+				canJump = false;
 			}
+				
+		}
+		if(coll.tag == "Page" || coll.tag == "Book")
+		{
+			Destroy (coll.gameObject);
 		}
 	}
 }
