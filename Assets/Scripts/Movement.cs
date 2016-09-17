cfead44c4853493277a5fe6a7f02f69f15e27b3e
AgaMatt <heitormattosinho@gmail.com>
2016-09-17 05:21:41 -0300

    Bouncing Fixed
    Now the player can just bounce when he starts falling

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,34 +4,42 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
 
-	public float forca, kickPage, kickBook, kickFloor;
-	public GameObject player;
-	bool canJump;
+	public float forca, kickPage, kickBook;
+	bool canJump, canBounce;
+	Rigidbody2D _rb;
+	public Vector2 playerSpeed;
 
+	void Start()
+	{	
+		canBounce = true;
+		_rb = GetComponent<Rigidbody2D> ();
+
+	}
 
 	void Update ()
 	{
-		
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			canJump = true;
-			//	print ("AAAA");
-			GetComponent<Rigidbody2D> ().AddForce (Vector3.down * forca * Time.deltaTime);
+		playerSpeed = _rb.velocity;
+		Bounce ();
+		if(Vector2.Distance (playerSpeed, new Vector2(0,0)) < 0.1){
+			canBounce = true;
+			print ("CAN BOUNCE");
+		}else{
+			//print ("CANT");
 		}
+
 	}
 
 	void OnTriggerEnter2D (Collider2D coll)
 	{
-		//print ("Colili");
-		if(coll.tag == "Floor"){
-			canJump = false;
-		}
-		if (canJump) {
+		if (canBounce) {
 			if (coll.tag == "Page") {
 				//print ("AloPAGE");
-				gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * kickPage * Time.deltaTime);
+				gameObject.GetComponent<Rigidbody2D> ().velocity = (Vector3.up * kickPage * Time.deltaTime);
+				canBounce = false;
 				canJump = false;
 			}else if(coll.tag == "Book"){
-				gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * kickBook * Time.deltaTime);
+				gameObject.GetComponent<Rigidbody2D> ().velocity = (Vector3.up * kickBook * Time.deltaTime);
+				canBounce = false;
 				canJump = false;
 			}
 				
@@ -39,6 +47,21 @@ public class Movement : MonoBehaviour
 		if(coll.tag == "Page" || coll.tag == "Book")
 		{
 			Destroy (coll.gameObject);
+		}
+	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		//print ("AAAAAAAAAAS"); 
+		canJump = false;
+		canBounce = false;
+	}
+
+	void Bounce()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			canJump = true;
+			//	print ("AAAA");
+			GetComponent<Rigidbody2D> ().velocity = (Vector3.down * forca * Time.deltaTime);
 		}
 	}
 }
